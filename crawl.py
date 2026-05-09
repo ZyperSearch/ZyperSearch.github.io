9ac5554cea81c7fa3dffd003d723e60f0b04ca84
Z-TEAM-OFFICIAL <eric.leslie.ford14@gmail.com>
2026-05-09 19:21:27 +0100

    Add more URLs to SEEDS list in crawl.py
    Extended the SEEDS list with additional URLs across various categories including finance, gaming, and health.

--- crawl.py
+++ crawl.py
@@ -4,71 +4,94 @@ import httpx
 import asyncio
 from bs4 import BeautifulSoup
 
+# ----------------------------------------------------------------------
+# ORIGINAL SEEDS (keep the block you already have – we will extend it)
+# ----------------------------------------------------------------------
 SEEDS = [
-
     "https://google.com", "https://bing.com", "https://yahoo.com", "https://duckduckgo.com", "https://baidu.com",
-
     "https://yandex.com", "https://wikipedia.org", "https://britannica.com", "https://archive.org", "https://quora.com",
-
     "https://medium.com", "https://substack.com", "https://wikihow.com", "https://stackexchange.com", "https://investopedia.com",
-
     "https://imdb.com", "https://bbc.com", "https://nytimes.com", "https://reuters.com", "https://theguardian.com",
-
     "https://cnn.com", "https://aljazeera.com", "https://bloomberg.com", "https://wsj.com", "https://forbes.com",
-
     "https://economist.com", "https://amazon.com", "https://ebay.com", "https://walmart.com", "https://target.com",
-
     "https://homedepot.com", "https://bestbuy.com", "https://costco.com", "https://etsy.com", "https://wayfair.com",
-
     "https://ikea.com", "https://alibaba.com", "https://aliexpress.com", "https://rakuten.co.jp", "https://mercadolivre.com.br",
-
     "https://jd.com", "https://shopify.com", "https://zillow.com", "https://realtor.com", "https://autotrader.com",
-
     "https://craigslist.org", "https://facebook.com", "https://instagram.com", "https://x.com", "https://linkedin.com",
-
     "https://reddit.com", "https://pinterest.com", "https://tumblr.com", "https://discord.com", "https://tiktok.com",
-
     "https://snapchat.com", "https://youtube.com", "https://vimeo.com", "https://twitch.tv", "https://netflix.com",
-
     "https://spotify.com", "https://soundcloud.com", "https://flickr.com", "https://behance.net", "https://dribbble.com",
-
     "https://deviantart.com", "https://apple.com", "https://microsoft.com", "https://cloudflare.com", "https://github.com",
-
     "https://gitlab.com", "https://bitbucket.org", "https://sourceforge.net", "https://npmjs.com", "https://pypi.org",
-
-    "https://docker.com", "https://aws.amazon.com", "https://azure.microsoft.com", "https://cloud.google.com", "https://digitalocean.com",
-
-    "https://heroku.com", "https://netlify.com", "https://vercel.com", "https://wordpress.org", "https://wix.com",
-
-    "https://squarespace.com", "https://godaddy.com", "https://bluehost.com", "https://namecheap.com", "https://cloudflare.com",
-
-    "https://weather.com", "https://accuweather.com", "https://wunderground.com", "https://nationalgeographic.com", "https://discovery.com",
-
-    "https://nasa.gov", "https://nih.gov", "https://cdc.gov", "https://who.int", "https://un.org",
-
+    "https://docker.com", "https://aws.amazon.com", "https://azure.microsoft.com", "https://cloud.google.com",
+    "https://digitalocean.com", "https://heroku.com", "https://netlify.com", "https://vercel.com", "https://wordpress.org",
+    "https://wix.com", "https://squarespace.com", "https://godaddy.com", "https://bluehost.com", "https://namecheap.com",
+    "https://weather.com", "https://accuweather.com", "https://wunderground.com", "https://nationalgeographic.com",
+    "https://discovery.com", "https://nasa.gov", "https://nih.gov", "https://cdc.gov", "https://who.int", "https://un.org",
     "https://whitehouse.gov", "https://europa.eu", "https://gov.uk", "https://ca.gov", "https://tokyo.jp",
-
     "https://booking.com", "https://expedia.com", "https://tripadvisor.com", "https://airbnb.com", "https://kayak.com",
-
     "https://skyscanner.net", "https://hotels.com", "https://yelp.com", "https://foursquare.com", "https://opentable.com",
-
     "https://uber.com", "https://lyft.com", "https://doordash.com", "https://instacart.com", "https://grubhub.com",
-
     "https://nike.com", "https://adidas.com", "https://zara.com", "https://h_m.com", "https://uniqlo.com",
-
     "https://gap.com", "https://nordstrom.com", "https://macys.com", "https://sephora.com", "https://ulta.com",
-
     "https://espn.com", "https://bleacherreport.com", "https://cbssports.com", "https://nfl.com", "https://nba.com",
-
     "https://mlb.com", "https://fifa.com", "https://olympics.com", "https://strava.com", "https://fitbit.com",
-
     "https://coursera.org", "https://udemy.com", "https://edx.org", "https://khanacademy.org", "https://duolingo.com",
-
-    "https://codecademy.com", "https://skillshare.com", "https://masterclass.com", "https://ted.com", "https://grammarly.com"
-
+    "https://codecademy.com", "https://skillshare.com", "https://masterclass.com", "https://ted.com", "https://grammarly.com",
+    
+    # --- ADDED 100 MORE (kept for context) ---
+    # Finance & Banking
+    "https://paypal.com", "https://stripe.com", "https://visa.com", "https://mastercard.com", "https://vanguard.com",
+    "https://fidelity.com", "https://morningstar.com", "https://marketwatch.com", "https://tradingview.com",
+    "https://coinbase.com", "https://binance.com", "https://robinhood.com", "https://mint.intuit.com",
+    "https://nerdwallet.com", "https://creditkarma.com",
+    
+    # Gaming & Entertainment
+    "https://steampowered.com", "https://epicgames.com", "https://ign.com", "https://gamespot.com",
+    "https://roblox.com", "https://riotgames.com", "https://nintendo.com", "https://playstation.com",
+    "https://xbox.com", "https://unity.com", "https://unrealengine.com", "https://twitch.tv",
+    "https://rottentomatoes.com", "https://metacritic.com",
+    
+    # Health & Wellness
+    "https://mayoclinic.org", "https://webmd.com", "https://healthline.com", "https://psychologytoday.com",
+    "https://medscape.com", "https://everydayhealth.com", "https://medicalnewstoday.com",
+    "https://sleepfoundation.org", "https://headspace.com", "https://calm.com", "https://myfitnesspal.com",
+    "https://healthgrades.com", "https://zocdoc.com", "https://drugs.com",
+        # SaaS & Productivity
+    "https://salesforce.com", "https://slack.com", "https://zoom.us", "https://trello.com",
+    "https://notion.so", "https://canva.com", "https://monday.com", "https://asana.com",
+    "https://dropbox.com", "https://evernote.com", "https://microsoft365.com", "https://google.workspace",
+    "https://calendly.com", "https://hubspot.com",
+    
+    # Tech News & Blogs
+    "https://wired.com", "https://techcrunch.com", "https://theverge.com", "https://engadget.com",
+    "https://gizmodo.com", "https://arstechnica.com", "https://cnet.com", "https://zdnet.com",
+    "https://venturebeat.com", "https://mashable.com", "https://digitaltrends.com",
+    "https://tomshardware.com", "https://9to5mac.com", "https://androidcentral.com",
+    
+    # Academic & Research
+    "https://jstor.org", "https://researchgate.net", "https://academia.edu", "https://mit.edu",
+    "https://stanford.edu", "https://harvard.edu", "https://ox.ac.uk", "https://cam.ac.uk",
+    "https://nature.com", "https://sciencemag.org", "https://arxiv.org", "https://scholar.google.com",
+    "https://springer.com", "https://wiley.com", "https://elsevier.com",
+    
+    # General News & Opinion
+    "https://vox.com", "https://huffpost.com", "https://slate.com", "https://salon.com",
+    "https://theatlantic.com", "https://newyorker.com", "https://time.com", "https://usnews.com",
+    "https://politico.com", "https://axios.com", "https://dailymail.co.uk", "https://foxnews.com",
+    "https://msnbc.com", "https://usatoday.com", "https://independent.co.uk",
+        # International Orgs & Gov
+    "https://imf.org", "https://worldbank.org", "https://wto.org", "https://nato.int",
+    "https://redcross.org", "https://amnesty.org", "https://greenpeace.org", "https://icrc.org",
+    "https://wmo.int", "https://unesco.org",
+    
+    # Lifestyle & Misc
+    "https://allrecipes.com", "https://foodnetwork.com", "https://bonappetit.com",
+    "https://goodhousekeeping.com", "https://realsimple.com", "https://apartmenttherapy.com",
+    "https://dwell.com"
 ]
 
-MODEL_URL = "https://api-inference.huggingface.co/models/Qwen/Qwen2.5-1.5B-Instruct"
+MODEL_URL = "https://api-inference.huggingface.co/models/ZyperAI/Z-AI-0.1-1.1B-Code.web"
 HEADERS = {"Authorization": "Bearer YOUR_HF_TOKEN"}
 
 async def get_ai_summary(client, text):
